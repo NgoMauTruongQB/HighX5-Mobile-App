f19a80e9bd37c0225a47b42f8dce7f30e8a09501
NgoMauTruongQB <truongngo2707@gmail.com>
2023-11-21 17:48:35 +0700

    notification

--- Frontend/screens/notifications/NotificationList.js
+++ Frontend/screens/notifications/NotificationList.js
@@ -1,12 +1,82 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import colors from '../../constants/colors'
 
 export default function NotificationList() {
+    const [categories, setCategories] = useState([
+        'All', 'Verified', 'Message', 'Event', 'Deadline'
+    ])
+    const [activeIndex, setActiveIndex] = useState(0)
+
+    const renderItem = ({ item, index }) => {
+        return (
+            <TouchableOpacity
+                onPress={() => setActiveIndex(index)}
+                style={[
+                    styles.categoryItem,
+                    index === activeIndex && styles.activeCategoryItem
+                ]}
+            >
+                <Text style={[styles.categoryText, index === activeIndex && styles.activeCategoryText]}>{item}</Text>
+            </TouchableOpacity>
+        )
+    }
+
     return (
-        <View>
-            <Text>NotificationList</Text>
+        <View style={styles.container}>
+            <FlatList
+                data={categories}
+                keyExtractor={(item, index) => index.toString()}
+                horizontal={true}
+                style={styles.category}
+                showsHorizontalScrollIndicator={false}
+                renderItem={renderItem}
+            />
+            <View style={styles.list}>
+                
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // backgroundColor: colors.light_gray,
+        paddingTop: 50,
+        flexDirection: 'column',
+    },
+    category: {
+        flex: 2,
+        flexDirection: 'row',
+        backgroundColor: colors.white,
+        paddingTop: 20,
+        paddingStart: 10
+    },
+    categoryItem: {
+        backgroundColor: colors.white,
+        marginHorizontal: 4,
+        height: 36,
+        minWidth: 120,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        borderWidth: 0.6,
+        borderColor: colors.dark_gray
+    },
+    activeCategoryItem: {
+        backgroundColor: colors.primary,
+        borderColor: colors.primary
+    },
+    categoryText: {
+        color: colors.text,
+        fontWeight: '600',
+        fontSize: 16,
+    },
+    activeCategoryText: {
+        color: colors.white,
+    },
+    list: {
+        flex: 15,
+    },
+})
