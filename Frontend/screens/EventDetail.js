b9ef6ab87665d616243c497903ac8cc50e3fb315
NgoMauTruongQB <truongngo2707@gmail.com>
2023-12-07 21:05:07 +0700

    tmp ed

--- Frontend/screens/EventDetail.js
+++ Frontend/screens/EventDetail.js
@@ -1,14 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
 const EventDetail = () => {
     return (
-        <View>
-            <Text>EventDetail</Text>
+        <View style={styles.container}>
+            {/* <Image source={require('')}  /> */}
         </View>
     )
 }
 
 export default EventDetail;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
