3a5ee577f84a53fdecc955c1b3e753c3bfb7cabd
Oreki1001 <trankimhieu1001@gmail.com>
2023-12-12 12:27:32 +0700

    update noti

--- Backend/seeders/20231212030332-notification-detail.js
+++ Backend/seeders/20231212030332-notification-detail.js
@@ -8,15 +8,35 @@ module.exports = {
       [
           {
             noti_id : 1,
-	          user_id : 4
+	          user_id : 1,
           },
           {
             noti_id : 2,
-	          user_id : 2
+	          user_id : 1,
           },
           {
             noti_id : 3,
-	          user_id : 3
+	          user_id : 1,
+          },
+          {
+            noti_id : 4,
+	          user_id : 2,
+          },
+          {
+            noti_id : 5,
+	          user_id : 3,
+          },
+          {
+            noti_id : 6,
+	          user_id : 2,
+          },
+          {
+            noti_id : 7,
+	          user_id : 4,
+          },
+          {
+            noti_id : 8,
+	          user_id : 5,
           },
       ],
       {}
