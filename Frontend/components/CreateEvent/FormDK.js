e773c1e06dee17830a9a7eb38db904b21b0961ce
Ngoc-Quyen <115072455+Ngoc-Quyen@users.noreply.github.com>
2023-12-12 12:09:12 +0700

    update NewEvent, UpdateProfile

---
+++ Frontend/components/CreateEvent/FormDK.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const FormDK = () => {
+    return (
+        <View>
+            <Text>FormDK</Text>
+        </View>
+    );
+};
+
+export default FormDK;
+
+const styles = StyleSheet.create({});
