18fd75247f41414ffdfb08aab83c4c31c301c1af
DungNguyen1702 <vandung17022003@gmail.com>
2023-12-14 15:53:28 +0700

    change api activity

--- Backend/controllers/api/activity.controller.js
+++ Backend/controllers/api/activity.controller.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { createActivity, findActivityByUserId, updateActivity, findActivityByID, findActivityByEventID } = require("../CRUD/activity");
+const { createActivity, findActivityByUserId, updateActivity, findActivityByID, findActivityByEventID, findAllActivityByUserId, findAllActivityByEventID } = require("../CRUD/activity");
 const objectCleaner = require("../../helpers/object-cleaner");
 
 async function addActivity(request, response) {
@@ -29,17 +29,17 @@ async function addActivity(request, response) {
 async function getActivityByUser(request, response)
 {
     try {
-        const { status , user_id } = request.body;
+        const { status , user_id } = request.query;
 
         var result;
 
-        if(status !== 2)
+        if(status != 2)
         {
             result = await findActivityByUserId(user_id, status)
         }
         else
         {
-            result = await findActivityByUserId(user_id);   
+            result = await findAllActivityByUserId(user_id);   
         }
 
         return response.status(200).json(result);
@@ -54,17 +54,18 @@ async function getActivityByUser(request, response)
 async function getActivityByEvent(request, response)
 {
     try {
-        const { status , event_id } = request.body;
+        const { status , event_id } = request.query;
 
         var result;
 
-        if(status !== 2)
+        if(status != 2)
         {
             result = await findActivityByEventID(event_id, status)
         }
         else
         {
-            result = await findActivityByEventID(event_id);   
+            console.log(2)
+            result = await findAllActivityByEventID(event_id);   
         }
 
         return response.status(200).json(result);
